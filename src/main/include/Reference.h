25ced99cdaf4fafb922eb357337469f40676d2f8
2826WaveRobotics <2826WaveRobotics@users.noreply.github.com>
2022-03-23 20:26:03 -0500

    "initial commit of rapid react 2022 robot code"

---
+++ src/main/include/Reference.h
@@ -0,0 +1,37 @@
+#ifndef REFERENCE_H
+#define REFERENCE_H
+
+#define COMP_BOT
+
+#ifdef COMP_BOT
+
+#define CLIMBER_ROLLER                  52  // config
+#define HOOK_DEPLOY                     3   // config
+#define GRASSHOPPER_DRIVE               12  // config
+#define RIGHT_GRASSHOPPER_DEPLOY        1   // config
+#define LEFT_GRASSHOPPER_DEPLOY         2   // config
+#define IS_RIGHT_GRASSHOPPER_DEPLOYED   7
+#define IS_LEFT_GRASSHOPPER_DEPLOYED    4
+
+#define BACK_PUNCHER                    17  // config
+#define FRONT_PUNCHER                   16  // config
+#define BACK_CAM_SENSOR                 1   // config
+#define FRONT_CAM_SENSOR                0   // config
+
+#define INTAKE_ROLLER                   59  // config
+#define INTAKE_DEPLOY                   58  // config
+#define IS_INTAKE_STOWED                2
+#define IS_INTAKE_DEPLOYED              3   // config
+
+#define TRANSPORTER_BELT                43  // config
+#define RIGHT_INDEXER                   15  // config
+#define LEFT_INDEXER                    18  // config
+
+#define RIGHT_FRONT_DRIVE               7   // config
+#define RIGHT_BACK_DRIVE                6   // config
+#define LEFT_FRONT_DRIVE                4   // config
+#define LEFT_BACK_DRIVE                 5   // config
+
+#endif // End of COMP_BOT
+
+#endif // END
